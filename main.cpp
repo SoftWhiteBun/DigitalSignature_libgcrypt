4f30f6c32944925c6da151dd97749ab6b52bf15c
Baranovich.S <baranovich.sofya@yandex.ru>
2023-07-25 17:35:42 +0300

    Функции меню разбиты на вспомогательные функции

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 #define BUFFER_SIZE 4096
 enum EERORRS {
-    CREATE_SEXPR_ERROR = 1,
+    CREATE_SEXP_ERROR = 1,
     CREATE_KEY_ERROR,
     PRIVATE_KEY_MISSING,
     PUBLIC_KEY_MISSING,
@@ -18,15 +18,39 @@ enum EERORRS {
     INVALID_PARAMETRS_ERROR
 };
 
-static void show_sexp (gcry_sexp_t a, FILE* SignFile)
-{
+static void show_sexp (gcry_sexp_t sexp, FILE* file) {
   char *buf;
   size_t size;
-  size = gcry_sexp_sprint (a, GCRYSEXP_FMT_ADVANCED, NULL, 0);
+  size = gcry_sexp_sprint (sexp, GCRYSEXP_FMT_ADVANCED, NULL, 0);
   buf = (char*)gcry_xmalloc (size);
-  gcry_sexp_sprint (a, GCRYSEXP_FMT_ADVANCED, buf, size);
-  fprintf (SignFile, "%.*s", (int)size, buf);
+  gcry_sexp_sprint (sexp, GCRYSEXP_FMT_ADVANCED, buf, size);
+  fprintf (file, "%.*s", (int)size, buf);
   gcry_free (buf);
+}
+
+gcry_sexp_t readSexpFile(FILE* file) {
+    gcry_sexp_t sexp;
+    gpg_error_t errorCode;
+
+    fseek( file, 0, SEEK_END );
+    long file_size = ftell( file );
+    char * buffer = (char*) malloc( file_size );
+    if( !buffer ) {
+        fclose( file );
+        fputs( "Could not allocate memory for file buffer. File could be empty or too large.", stderr );
+        exit(ALLOCATE_ERROR);
+    }
+    fseek ( file , 0, SEEK_SET );
+    fread( buffer, 1, file_size, file );
+
+    errorCode = gcry_sexp_new(&sexp, buffer, strlen(buffer), 0);
+    if (errorCode) {
+        fprintf(stdout, "Creating S-expression failed: %s\n", gcry_strerror (errorCode));
+        exit(CREATE_SEXP_ERROR);
+    }
+    free(buffer);
+
+    return sexp;
 }
 
 std::pair<gcry_sexp_t, gcry_sexp_t> KeyPairGenerate(char* argv[]) {
@@ -38,7 +62,7 @@ std::pair<gcry_sexp_t, gcry_sexp_t> KeyPairGenerate(char* argv[]) {
     errorCode = gcry_sexp_build(&keySpec, NULL, "(genkey (dsa (nbits 4:1024)))");
     if (errorCode) {
          fprintf(stdout, "creating S-expression failed: %s\n", gcry_strerror (errorCode));
-        exit(CREATE_SEXPR_ERROR);
+        exit(CREATE_SEXP_ERROR);
     }
 
     errorCode = gcry_pk_genkey (&keyPairSexp, keySpec);
@@ -64,26 +88,9 @@ std::pair<gcry_sexp_t, gcry_sexp_t> KeyPairGenerate(char* argv[]) {
     return keyPair;
 }
 
-void DigitalSignatureGenerate(char* argv[], gcry_sexp_t secretKey) {
-    gcry_sexp_t DigitalSignature;
-    gpg_error_t errorCode;
-
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        printf("Error opening file %s\n", argv[1]);
-        exit(OPEN_FILE_ERROR);
-    }
-
-    FILE *SignFile = fopen(argv[2], "w");
-    if (SignFile == NULL) {
-        printf("Error opening file %s\n", argv[2]);
-        exit(OPEN_FILE_ERROR);
-    }
-
-    //--------------- ХЕШИРОВАНИЕ
-
-    unsigned char * hash;
-    gcry_sexp_t data;
+gcry_sexp_t HashGenerate(FILE* file) {
+    unsigned char * data;
+    gcry_sexp_t hash;
 
     gcry_md_hd_t hd;
     gcry_md_open(&hd, GCRY_MD_SHA256, 0);
@@ -94,40 +101,73 @@ void DigitalSignatureGenerate(char* argv[], gcry_sexp_t secretKey) {
         gcry_md_write(hd, buffer, nread);
     }
 
-    hash = gcry_md_read(hd, GCRY_MD_SHA256);
+    data = gcry_md_read(hd, GCRY_MD_SHA256);
     fprintf(stdout, "\nHash: ");
     for (int i = 0; i < gcry_md_get_algo_dlen(GCRY_MD_SHA256); i++) {
-        fprintf(stdout, "%c", hash[i]);
+        fprintf(stdout, "%c", data[i]);
         fflush(stdout);
     }
 
     fprintf(stdout, "\n");
 
-    gcry_sexp_build(&data, NULL, "(data (flags raw) (value %s))", hash);
+    gcry_sexp_build(&hash, NULL, "(data (flags raw) (value %s))", data);
 
-    //---------------- ЭЦП
+    gcry_md_close(hd);
 
-    errorCode = gcry_pk_sign(&DigitalSignature, data, secretKey);
+    return hash;
+}
+
+void DigitalSignatureGenerate(char* argv[]) {
+    gcry_sexp_t DigitalSignature;
+    gpg_error_t errorCode;
+    gcry_sexp_t hash;
+
+    FILE *DataFile = fopen(argv[1], "rb");
+    if (DataFile == NULL) {
+        printf("Error opening file %s\n", argv[1]);
+        exit(OPEN_FILE_ERROR);
+    }
+
+    FILE *SignFile = fopen(argv[2], "w");
+    if (SignFile == NULL) {
+        printf("Error opening file %s\n", argv[2]);
+        exit(OPEN_FILE_ERROR);
+    }
+
+    FILE *PubKeyFile = fopen(argv[3], "w");
+    if (PubKeyFile == NULL) {
+        printf("Error opening file %s\n", argv[3]);
+        exit(OPEN_FILE_ERROR);
+    }
+
+    auto [secretKey, publicKey] = KeyPairGenerate(argv); // генерация ключей
+    show_sexp(publicKey, PubKeyFile);
+
+    hash = HashGenerate(DataFile); // хеширование
+
+    errorCode = gcry_pk_sign(&DigitalSignature, hash, secretKey); // генерация ЭЦП
     if (errorCode) {
         fprintf(stdout, "Signing faild: %s\n", gcry_strerror(errorCode));
         exit(SIGNING_ERROR);
     }
-    cout << endl;
-    show_sexp(DigitalSignature, stdout);
-    cout << endl;
+
     show_sexp(DigitalSignature, SignFile);
 
-    fclose(file);
+    cout << "Digital signature generated success" << endl;
+
+    fclose(DataFile);
     fclose(SignFile);
-    gcry_md_close(hd);
+    fclose(PubKeyFile);
 }
 
-void DigitalSignatureVerification(char* argv[], gcry_sexp_t publicKey) {
+void DigitalSignatureVerification(char* argv[]) {
     gpg_error_t errorCode;
+    gcry_sexp_t publicKey;
+    gcry_sexp_t hash;
     gcry_sexp_t signature;
 
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL) {
+    FILE *DataFile = fopen(argv[1], "rb");
+    if (DataFile == NULL) {
         printf("Error opening file %s\n", argv[1]);
         exit(OPEN_FILE_ERROR);
     }
@@ -138,93 +178,52 @@ void DigitalSignatureVerification(char* argv[], gcry_sexp_t publicKey) {
         exit(OPEN_FILE_ERROR);
     }
 
-    //---------------- ЧТЕНИЕ СИГНАТУРЫ ИЗ ФАЙЛА
-
-    fseek( SignFile, 0, SEEK_END );
-    long file_size = ftell( SignFile );
-    char * buffer = (char*) malloc( file_size );
-    if( !buffer ) {
-        fclose( SignFile );
-        fputs( "Could not allocate memory for file buffer. File could be empty or too large.", stderr );
-        exit(ALLOCATE_ERROR);
-    }
-    fseek ( SignFile , 0, SEEK_SET );
-    fread( buffer, 1, file_size, SignFile );
-
-    errorCode = gcry_sexp_new(&signature, buffer, strlen(buffer), 0);
-    if (errorCode) {
-        fprintf(stdout, "creating S-expression failed: %s\n", gcry_strerror (errorCode));
-        exit(CREATE_SEXPR_ERROR);
+    FILE *PubKeyFile = fopen(argv[3], "r");
+    if (PubKeyFile == NULL) {
+        printf("Error opening file %s\n", argv[3]);
+        exit(OPEN_FILE_ERROR);
     }
 
-    //-------------- НОВЫЙ ХЕШ
+    signature = readSexpFile(SignFile);
+    publicKey = readSexpFile(PubKeyFile);
 
-    gcry_md_hd_t new_hd;
-    gcry_md_open(&new_hd, GCRY_MD_SHA256, 0);
+    hash = HashGenerate(DataFile); // хеширование
 
-    unsigned char new_buffer[BUFFER_SIZE]="";
-    size_t new_nread;
-
-    while ((new_nread = fread(new_buffer, 1, sizeof(new_buffer), file)) > 0) {
-        gcry_md_write(new_hd, new_buffer, new_nread);
-    }
-
-    unsigned char * new_hash = gcry_md_read(new_hd, GCRY_MD_SHA256);
-    fprintf(stdout, "\nHash: ");
-    for (int i = 0; i < gcry_md_get_algo_dlen(GCRY_MD_SHA256); i++) {
-        fprintf(stdout, "%c", new_hash[i]);
-    }
-    fflush(stdout);
-    fprintf(stdout, "\n");
-
-    gcry_sexp_t new_data;
-    gcry_sexp_build(&new_data, NULL, "(data (flags raw) (value %s))", new_hash);
-
-    //--------------- ПРОВЕРКА
-
-    errorCode = gcry_pk_verify(signature, new_data, publicKey);
+    errorCode = gcry_pk_verify(signature, hash, publicKey); // верификация
     if (errorCode) {
         fprintf(stdout, "verify faild: %s\n\n", gcry_strerror(errorCode));
-        fprintf(stdout, (const char*)new_buffer);
-    } else fprintf(stdout, "SUCCESS\n");
+    } else fprintf(stdout, "VERIFY SUCCESS\n");
 
-    gcry_md_close(new_hd);
-    fclose(file);
+    fclose(DataFile);
     fclose(SignFile);
+    fclose(PubKeyFile);
 
 }
 
 int main(int argc, char* argv[]) {
 
-    if (argc != 3) {
+    if (argc != 4) {
         printf("Usage: %s <filename>\n", argv[0]);
         exit(INVALID_PARAMETRS_ERROR);
     }
 
     bool UExit = false;
     char UMenu;
-
-    //--------------- ГЕНЕРАЦИЯ КЛЮЧА
-
-    auto [secretKey, publicKey] = KeyPairGenerate(argv); // генерация ключа должна быть в генерации ЭЦП + запись в файл публичной части
-
-    // ------------------------ МЕНЮ -------------------------------
-
         while (!UExit) {
             cout << "Menu: " << endl;
             cout << "Print char to:" << endl;
             cout << "g - to generate EDS" << endl;
-            cout << "v - to verify EDS" << endl; // чтение публичного ключа из файла
+            cout << "v - to verify EDS" << endl;
             cout << "e - to exit" << endl;
 
             cin >> UMenu;
 
             if (UMenu == 'g') {
-                DigitalSignatureGenerate(argv, secretKey);
+                DigitalSignatureGenerate(argv);
             }
 
             else if (UMenu == 'v') {
-                DigitalSignatureVerification(argv, publicKey);
+                DigitalSignatureVerification(argv);
             }
 
             else if (UMenu == 'e')
